cd2a53de1343f155d08c24c24bf8636feca9a4fd
VKNagibin <itisnotstupid@gmail.com>
2024-05-12 21:23:32 +0300

    add types.ts

---
+++ src/types.ts
@@ -0,0 +1,4 @@
+import { CELL_CHAR, CELL_NUMBER } from './constants';
+
+export type CharValueType = (typeof CELL_CHAR)[keyof typeof CELL_CHAR];
+export type NumberValueType = (typeof CELL_NUMBER)[keyof typeof CELL_NUMBER];
