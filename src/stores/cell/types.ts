1ddf4e9993bd70d3ce97e61f9e245e4ad3be3442
VKNagibin <itisnotstupid@gmail.com>
2024-05-26 19:07:43 +0300

    add cell types

---
+++ src/stores/cell/types.ts
@@ -0,0 +1,7 @@
+import { HighlightType } from '@/entities/Cell/enums';
+import { CellIdType } from '@/entities/Cell/types';
+
+export interface IStep {
+  cellId: CellIdType;
+  highlight: HighlightType;
+}
