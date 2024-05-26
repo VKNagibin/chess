22fe2451131f38a6d868f9b729aaecaf8b43d0da
VKNagibin <itisnotstupid@gmail.com>
2024-05-26 19:02:50 +0300

    update Cell component

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,33 +1,47 @@
 import './index.less';
 
 import LazySvg from '@components/LazySvg';
-import { memo, useEffect, useMemo } from 'react';
+import { useUnit } from 'effector-react';
 
-import Board from '@/Board';
-import CellClass from '@/Cell';
-import { HighlightType } from '@/enums';
-import Figure from '@/Figure';
-import useForceUpdate from '@/hooks/useForceUpdate';
+import CellClass from '@/entities/Cell/Cell';
+import { FigureTeam, HighlightType } from '@/entities/Cell/enums';
+import Figure from '@/entities/Figure';
+import { $currentStepTeam, onCellFocus } from '@/stores/cell';
+import { checkIsStep } from '@/stores/cell/utils';
+
+const classByHighlightType: Record<HighlightType, string> = {
+  [HighlightType.SELECTED]: 'selected',
+  [HighlightType.KILL_STEP]: 'kill',
+  [HighlightType.DEFAULT_STEP]: 'step',
+  [HighlightType.NONE]: '',
+};
 
 interface ICellsProps {
   cell: CellClass;
 }
 
+const getHoverClass = (cell: CellClass, currentStepTeam: FigureTeam) => {
+  const isStep = checkIsStep(cell.highlight);
+  const isCurrentTeam = currentStepTeam === cell.figure?.team;
+  return isStep || isCurrentTeam ? 'filled' : '';
+};
+
 const getFigureSvgName = (figure: Figure) => `${figure.type}_${figure.team}`;
 
 const Cell = ({ cell }: ICellsProps) => {
-  const { changeHighlighted } = Board.getBoard();
-  const forceUpdate = useForceUpdate();
+  const [handleCellFocus, currentStepTeam] = useUnit([onCellFocus, $currentStepTeam]);
+
+  const hoverClass = getHoverClass(cell, currentStepTeam);
+
+  const tabIndex = cell.figure ? 0 : -1;
 
   return (
     <button
-      onClick={() => {
-        changeHighlighted(cell.id, forceUpdate);
-        forceUpdate();
-      }}
+      onClick={() => handleCellFocus({ cellId: cell.id, currentStepTeam })}
       className={`cell ${cell.color} ${
-        cell.highlight === HighlightType.SELECTED ? 'selected' : ''
-      }`}
+        classByHighlightType[cell.highlight]
+      } ${hoverClass}`}
+      tabIndex={tabIndex}
     >
       {cell.figure ? <LazySvg name={getFigureSvgName(cell.figure)} /> : null}
     </button>
