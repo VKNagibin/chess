5c1b406ba7e52ffa3ebbd022e62a5455d95b6963
VKNagibin <itisnotstupid@gmail.com>
2024-06-01 18:53:17 +0300

    add data.ts for cell component

---
+++ src/components/Cell/data.ts
@@ -0,0 +1,8 @@
+import { HighlightType } from '@/entities/Cell/enums';
+
+export const classByHighlightType: Record<HighlightType, string> = {
+  [HighlightType.SELECTED]: 'selected',
+  [HighlightType.KILL_STEP]: 'kill',
+  [HighlightType.DEFAULT_STEP]: 'step',
+  [HighlightType.NONE]: '',
+};
