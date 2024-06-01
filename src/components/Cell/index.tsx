9a49a02bfa3763e08e3a167c74fae3b0aa964f81
VKNagibin <itisnotstupid@gmail.com>
2024-06-01 18:53:35 +0300

    refactor Cell component

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,56 +1,23 @@
 import './index.less';
 
 import LazySvg from '@components/LazySvg';
-import { useUnit } from 'effector-react';
-import { useEffect } from 'react';
 
 import CellClass from '@/entities/Cell/Cell';
-import { FigureTeam, HighlightType } from '@/entities/Cell/enums';
-import Figure from '@/entities/Figure';
-import { $currentStepTeam, onCellFocus } from '@/stores/cell';
-import { checkIsStep } from '@/stores/cell/utils';
 
-const classByHighlightType: Record<HighlightType, string> = {
-  [HighlightType.SELECTED]: 'selected',
-  [HighlightType.KILL_STEP]: 'kill',
-  [HighlightType.DEFAULT_STEP]: 'step',
-  [HighlightType.NONE]: '',
-};
+import useCellLogic from './useCellLogic';
+import { getFigureSvgName } from './utils';
 
 interface ICellsProps {
   cell: CellClass;
 }
 
-const getHoverClass = (cell: CellClass, currentStepTeam: FigureTeam) => {
-  const isStep = checkIsStep(cell.highlight);
-  const isCurrentTeam = currentStepTeam === cell.figure?.team;
-  return isStep || isCurrentTeam ? 'filled' : '';
-};
-
-const getFigureSvgName = (figure: Figure) => `${figure.type}_${figure.team}`;
-
 const Cell = ({ cell }: ICellsProps) => {
-  const [handleCellFocus, currentStepTeam] = useUnit([onCellFocus, $currentStepTeam]);
-
-  const hoverClass = getHoverClass(cell, currentStepTeam);
-
-  const tabIndex = cell.figure ? 0 : -1;
-
-  useEffect(() => {
-    cell.isOver &&
-      alert(
-        `Game over! ${
-          cell.figure?.team === FigureTeam.BLACK ? FigureTeam.WHITE : FigureTeam.BLACK
-        } team win!`,
-      );
-  }, [cell.isOver]);
+  const { tabIndex, handleCellFocus, currentStepTeam, className } = useCellLogic(cell);
 
   return (
     <button
       onClick={() => handleCellFocus({ cellId: cell.id, currentStepTeam })}
-      className={`cell ${cell.color} ${
-        classByHighlightType[cell.highlight]
-      } ${hoverClass}`}
+      className={className}
       tabIndex={tabIndex}
     >
       {cell.figure && !cell.hiddenFigure ? (
