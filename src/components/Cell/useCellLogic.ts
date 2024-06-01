e8b522d0efeec9edc22f81425c970b5e17d7c1da
VKNagibin <itisnotstupid@gmail.com>
2024-06-01 18:53:51 +0300

    add useCellLogic hook

---
+++ src/components/Cell/useCellLogic.ts
@@ -0,0 +1,46 @@
+import { useUnit } from 'effector-react';
+import { useEffect } from 'react';
+
+import Cell from '@/entities/Cell/Cell';
+import { FigureTeam } from '@/entities/Cell/enums';
+import { $currentStepTeam, onCellFocus, onGameOver } from '@/stores/cell';
+
+import { classByHighlightType } from './data';
+import { getHoverClass } from './utils';
+
+export default function useCellLogic(cell: Cell) {
+  const [handleCellFocus, handleGameOver, currentStepTeam] = useUnit([
+    onCellFocus,
+    onGameOver,
+    $currentStepTeam,
+  ]);
+
+  const hoverClass = getHoverClass(cell, currentStepTeam);
+  const tabIndex = cell.figure ? 0 : -1;
+
+  const className = `cell ${cell.color} ${
+    classByHighlightType[cell.highlight]
+  } ${hoverClass}`;
+
+  useEffect(() => {
+    if (cell.isOver) {
+      setTimeout(() => {
+        handleGameOver();
+
+        alert(
+          `Game over! ${
+            cell.figure?.team === FigureTeam.BLACK ? FigureTeam.BLACK : FigureTeam.WHITE
+          } team win!`,
+        );
+      }, 200);
+    }
+  }, [cell.isOver]);
+
+  return {
+    hoverClass,
+    tabIndex,
+    currentStepTeam,
+    className,
+    handleCellFocus,
+  };
+}
