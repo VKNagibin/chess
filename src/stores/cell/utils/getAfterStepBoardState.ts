0efa58a9dd6577427ed7f4e3975e0aa595efc852
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 10:02:15 +0300

    moved getAfterStepBoardState.ts

---
+++ src/stores/cell/utils/getAfterStepBoardState.ts
@@ -0,0 +1,39 @@
+import Cell from '@/entities/Cell/Cell';
+import { FigureType, HighlightType } from '@/entities/Cell/enums';
+import Figure from '@/entities/Figure';
+import { resetCellsHighlight } from '@/stores/cell/utils';
+import handlePawnStep from '@/stores/cell/utils/handlePawnStep';
+
+interface IBoardState {
+  cells: Cell[];
+  currentFigure: Figure;
+}
+
+export default function (
+  currentCell: Cell,
+  cells: Cell[],
+  stepOwner?: Cell,
+): IBoardState {
+  const stepOwnerCell =
+    stepOwner || cells.find((cell) => cell.highlight === HighlightType.SELECTED)!;
+  if (!stepOwnerCell) throw new Error('Ups...Something went wrong...');
+  const currentFigure = stepOwnerCell.figure!;
+  const stepsWithoutHighlight = resetCellsHighlight(cells);
+
+  let afterStepCellsState: Cell[];
+
+  if (currentFigure.type === FigureType.PAWN)
+    afterStepCellsState = handlePawnStep(stepOwnerCell, currentCell, cells);
+  else
+    afterStepCellsState = stepsWithoutHighlight.map((cell) => {
+      if (cell.hiddenFigure) {
+        cell.hiddenFigure = false;
+        cell.figure = null;
+      }
+      if (cell.id === currentCell.id) return { ...cell, figure: currentFigure };
+      if (cell.id === stepOwnerCell!.id) return { ...cell, figure: null };
+      return cell;
+    });
+
+  return { cells: afterStepCellsState, currentFigure };
+}
