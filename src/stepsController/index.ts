415986f3f333ddd862491ddfbac2bf6edfb1ce2a
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 10:09:01 +0300

    update stepController index.ts

--- src/stepsController/index.ts
+++ src/stepsController/index.ts
@@ -1,28 +1,23 @@
 import Cell from '@/entities/Cell/Cell';
 import { FigureType } from '@/entities/Cell/enums';
-import handlePawnSteps from '@/stepsController/pawn';
+import handleBishopSteps from '@/stepsController/handlers/bishop';
+import handleKingSteps from '@/stepsController/handlers/king';
+import handleKnightSteps from '@/stepsController/handlers/knight';
+import handlePawnSteps from '@/stepsController/handlers/pawn';
+import handleQueenSteps from '@/stepsController/handlers/queen';
+import handleRookSteps from '@/stepsController/handlers/rook';
 import { IStep } from '@/stores/cell/types';
-
-import handleKnightSteps from './knight';
 
 const handlersByFigureTypeMap: Record<
   FigureType,
   (cells: Cell[], focusedCell: Cell) => IStep[]
 > = {
   [FigureType.PAWN]: handlePawnSteps,
-  [FigureType.KING]: () => {
-    return [];
-  },
-  [FigureType.QUEEN]: () => {
-    return [];
-  },
+  [FigureType.KING]: handleKingSteps,
+  [FigureType.QUEEN]: handleQueenSteps,
   [FigureType.KNIGHT]: handleKnightSteps,
-  [FigureType.BISHOP]: () => {
-    return [];
-  },
-  [FigureType.ROOK]: () => {
-    return [];
-  },
+  [FigureType.BISHOP]: handleBishopSteps,
+  [FigureType.ROOK]: handleRookSteps,
 };
 
 export default handlersByFigureTypeMap;
