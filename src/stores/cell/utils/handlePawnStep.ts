438ee1718935b2fe931143d2e4b07cfd7c07766c
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 10:03:25 +0300

    moved handlePawnStep.ts

---
+++ src/stores/cell/utils/handlePawnStep.ts
@@ -0,0 +1,42 @@
+import Cell from '@/entities/Cell/Cell';
+import { resetCellsHighlight } from '@/stores/cell/utils';
+
+export default function (stepOwner: Cell, currentCell: Cell, cells: Cell[]) {
+  const cellsWithoutHighlight = resetCellsHighlight(cells);
+  if (!currentCell.hiddenFigure)
+    return cellsWithoutHighlight.map((cell) => {
+      if (currentCell.enPassantCellId === cell.id) {
+        return { ...cell, figure: stepOwner.figure, hiddenFigure: true };
+      }
+      if (cell.hiddenFigure)
+        return {
+          ...cell,
+          hiddenFigure: false,
+          figure: null,
+        };
+
+      if (cell.id === currentCell.id)
+        return {
+          ...cell,
+          figure: stepOwner.figure,
+        };
+      if (cell.id === stepOwner.id) return { ...cell, figure: null };
+      return cell;
+    });
+
+  const sourceCell = cells.find(
+    (cell) => currentCell.figure?.id === cell.figure?.id && currentCell.id !== cell.id,
+  );
+
+  return cellsWithoutHighlight.map((cell) => {
+    if (cell.id === sourceCell?.id) return { ...cell, figure: null };
+    if (cell.id === currentCell.id)
+      return { ...cell, figure: stepOwner.figure, hiddenFigure: false };
+    if (stepOwner.id === cell.id)
+      return {
+        ...cell,
+        figure: null,
+      };
+    return cell;
+  });
+}
