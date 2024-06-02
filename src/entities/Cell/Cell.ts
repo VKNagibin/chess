5dde325597c4108c12358a0f5b106beb111f50c7
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 08:11:53 +0300

    update Cell.ts

--- src/entities/Cell/Cell.ts
+++ src/entities/Cell/Cell.ts
@@ -1,4 +1,4 @@
-import { CellColor, FigureType, HighlightType } from '@/entities/Cell/enums';
+import { CellColor, HighlightType } from '@/entities/Cell/enums';
 import { CellIdType } from '@/entities/Cell/types';
 import Figure from '@/entities/Figure';
 
@@ -10,11 +10,6 @@ class Cell {
   isOver = false;
 
   constructor(public id: CellIdType, public color: CellColor) {}
-
-  checkIsKing = (): boolean => {
-    if (!this.figure) return false;
-    return this.figure.type === FigureType.KING;
-  };
 }
 
 export default Cell;
