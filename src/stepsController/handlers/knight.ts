d46580babe0dac16b9a995e9905ba2730468f97e
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 10:08:06 +0300

    moved stepController knight.ts

--- src/stepsController/handlers/knight.ts
+++ src/stepsController/handlers/knight.ts
@@ -1,35 +1,11 @@
 import Cell from '@/entities/Cell/Cell';
-import { FigureTeam, HighlightType } from '@/entities/Cell/enums';
-import { CellIdType } from '@/entities/Cell/types';
 import { PotentialStepType } from '@/stepsController/types';
+import addStep from '@/stepsController/utils/addStep';
 import { IStep } from '@/stores/cell/types';
-import { findById } from '@/stores/cell/utils';
 
-import { teamHandlersMap } from './data';
+import { teamHandlersMap } from '../data';
 
-export function addStep(
-  targetCellId: CellIdType | null,
-  cells: Cell[],
-  steps: IStep[],
-  currentTeam: FigureTeam,
-): boolean {
-  if (!targetCellId) return false;
-  const targetCell = findById(cells, targetCellId) as Cell;
-  if (!targetCell) return false;
-  if (!targetCell.figure || targetCell.hiddenFigure) {
-    steps.push({
-      cellId: targetCellId,
-      highlight: HighlightType.DEFAULT_STEP,
-    });
-    return true;
-  }
-  if (targetCell.figure.team === currentTeam) return false;
-
-  steps.push({ cellId: targetCellId, highlight: HighlightType.KILL_STEP });
-  return true;
-}
-
-export default function handleKnightSteps(cells: Cell[], focusedCell: Cell) {
+export default function (cells: Cell[], focusedCell: Cell) {
   const steps: IStep[] = [];
   const currentTeam = focusedCell.figure!.team;
   const { forward, left, right, backward } = teamHandlersMap[currentTeam];
