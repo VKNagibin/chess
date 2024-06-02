b0b9a7a7c1fdd6b7448b7a76128e147792fc6222
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 15:57:48 +0300

    update cell store types.ts

--- src/stores/cell/types.ts
+++ src/stores/cell/types.ts
@@ -1,8 +1,16 @@
+import Cell from '@/entities/Cell/Cell';
 import { HighlightType } from '@/entities/Cell/enums';
 import { CellIdType } from '@/entities/Cell/types';
+
+export interface IAdditionalStep {
+  cellId: CellIdType;
+  highlight: HighlightType;
+  owner: Cell;
+}
 
 export interface IStep {
   cellId: CellIdType;
   highlight: HighlightType;
   enPassantCellId?: CellIdType;
+  additionalStep?: IAdditionalStep;
 }
