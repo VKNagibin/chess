f5651cfee06618cbad25780f12f6f9cfcd271178
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 08:12:50 +0300

    remove underAttack field from Figure.ts

--- src/entities/Figure.ts
+++ src/entities/Figure.ts
@@ -4,7 +4,6 @@ import { uniqId } from '@/utils';
 class Figure {
   id: string = uniqId();
   isFirstStep = true;
-  underAtack = false;
 
   constructor(public type: FigureType, public team: FigureTeam) {}
 }
