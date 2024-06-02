e8b351f68827b561e7dd839e1cb020c30cbb6208
VKNagibin <itisnotstupid@gmail.com>
2024-06-02 15:58:23 +0300

    update cell store handleStep.ts

--- src/stores/cell/utils/handleStep.ts
+++ src/stores/cell/utils/handleStep.ts
@@ -1,36 +1,68 @@
 import Cell from '@/entities/Cell/Cell';
 import { FigureTeam } from '@/entities/Cell/enums';
-import { getEnemyTeam, resetCellsHighlight } from '@/stores/cell/utils';
 import isKingCanBeSaved from '@/stores/cell/utils/checkIsKingCanBeSaved';
 import getAfterStepBoardState from '@/stores/cell/utils/getAfterStepBoardState';
 import getKingEnemies from '@/stores/cell/utils/getKingEnemies';
+import {
+  checkIsKing,
+  getEnemyTeam,
+  resetCellsHighlight,
+} from '@/stores/cell/utils/utils';
+
+function getKing(cells: Cell[], team: FigureTeam) {
+  return cells.find((cell) => cell.figure!.team === team && checkIsKing(cell));
+}
 
 export function handleStep(currentCell: Cell, cells: Cell[]): Cell[] {
   const { cells: preparedCells, currentFigure } = getAfterStepBoardState(
     currentCell,
     cells,
   );
+
+  let finalCells = preparedCells;
   const team = currentFigure.team;
+  const ourKing = getKing(preparedCells, team)!;
+  const theirKing = getKing(preparedCells, getEnemyTeam(team))!;
   const ourKingEnemies = getKingEnemies(preparedCells, team);
   const theirKingEnemies = getKingEnemies(preparedCells, getEnemyTeam(team));
 
   if (ourKingEnemies.length)
-    return getOurKingAttackResult(currentCell, team, preparedCells, cells);
+    return getOurKingAttackResult(ourKing, currentCell, team, preparedCells, cells);
+  else
+    finalCells = finalCells.map((cell) => {
+      if (cell.id === ourKing.id)
+        return { ...cell, figure: { ...cell.figure, isUnderAttack: false } } as Cell;
+      return cell;
+    });
 
-  if (theirKingEnemies.length) getTheirKingAttackResult(currentCell, team, preparedCells);
+  if (theirKingEnemies.length)
+    getTheirKingAttackResult(theirKing, currentCell, team, preparedCells);
+  else
+    finalCells = finalCells.map((cell) => {
+      if (cell.id === theirKing.id)
+        return { ...cell, figure: { ...cell.figure, isUnderAttack: false } } as Cell;
+      return cell;
+    });
 
   currentFigure.isFirstStep = false;
-  return preparedCells;
+  return finalCells;
 }
 
 function getOurKingAttackResult(
+  king: Cell,
   currentCell: Cell,
   team: FigureTeam,
-  preparedCells: Cell[],
+  afterStepCells: Cell[],
   cells: Cell[],
-) {
-  if (isKingCanBeSaved(cells, team)) return resetCellsHighlight(cells);
-  return preparedCells.map((cell) => {
+): Cell[] {
+  if (isKingCanBeSaved(cells, team)) {
+    return resetCellsHighlight(cells).map((cell) => {
+      if (cell.id === king?.id)
+        return { ...cell, figure: { ...cell.figure, isUnderAttack: true } } as Cell;
+      return cell;
+    });
+  }
+  return afterStepCells.map((cell) => {
     if (currentCell.id === cell.id)
       return {
         ...cell,
@@ -41,11 +73,18 @@ function getOurKingAttackResult(
 }
 
 function getTheirKingAttackResult(
+  king: Cell,
   currentCell: Cell,
   team: FigureTeam,
   preparedCells: Cell[],
 ) {
-  if (isKingCanBeSaved(preparedCells, getEnemyTeam(team))) return preparedCells;
+  if (isKingCanBeSaved(preparedCells, getEnemyTeam(team))) {
+    return preparedCells.map((cell) => {
+      if (cell.id === king?.id)
+        return { ...cell, figure: { ...cell.figure, isUnderAttack: true } } as Cell;
+      return cell;
+    });
+  }
   return preparedCells.map((cell) => {
     if (currentCell.id === cell.id)
       return {
