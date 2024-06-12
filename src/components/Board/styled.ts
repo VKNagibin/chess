52fbd3358725bca1151c7e0b614144befcf406eb
VKNagibin <itisnotstupid@gmail.com>
2024-06-12 15:53:47 +0300

    add StyledBoard

---
+++ src/components/Board/styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledBoardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledBoard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  background-color: white;
+  border: 10px solid black;
+  width: 640px;
+  height: 640px;
+  box-sizing: content-box;
+`;
