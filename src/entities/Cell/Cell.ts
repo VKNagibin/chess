ecf22cfb440ccdea1f81ce8b4a90c2d8b902bae1
VKNagibin <itisnotstupid@gmail.com>
2024-06-12 07:45:43 +0300

    update Cell entity

--- src/entities/Cell/Cell.ts
+++ src/entities/Cell/Cell.ts
@@ -2,8 +2,17 @@ import { CellColor, HighlightType } from '@/entities/Cell/enums';
 import { CellIdType } from '@/entities/Cell/types';
 import Figure from '@/entities/Figure';
 
+export interface ICastling {
+  targetCellId: CellIdType;
+  dependent: {
+    ownerCell: Cell;
+    targetCell: Cell;
+  };
+}
+
 class Cell {
   enPassantCellId: CellIdType | null = null;
+  castling: ICastling[] = [];
   hiddenFigure = false;
   highlight: HighlightType = HighlightType.NONE;
   figure: Figure | null = null;
