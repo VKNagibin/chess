7be0893b8750f18ecdf24908594612e53578c7f7
VKNagibin <itisnotstupid@gmail.com>
2024-06-12 15:55:27 +0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from '@/components/App/App';
+import { GlobalStyles } from '@/global.styled';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
