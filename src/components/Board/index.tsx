ae0b672a045ad0f9045884b6519d3aed087f1315
VKNagibin <itisnotstupid@gmail.com>
2024-06-12 15:57:23 +0300

    update Board component

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,22 @@
+import { StyledBoard, StyledBoardWrapper } from '@components/Board/styled';
+import Cells from '@components/Cells';
+import Chars from '@components/Chars';
+import Numbers from '@components/Numbers';
+
+function Board() {
+  return {};
+  //   <Numbers />
+  //   <StyledBoardWrapper>
+  //     <Chars />
+  //     <StyledBoard>
+  //       <Cells />
+  //     </StyledBoard>
+  //     <Numbers className="append" />
+  //     <Chars append />
+  //   </StyledBoardWrapper>
+  // <Numbers />
+
+  // );
+}
+
+export default Board;
