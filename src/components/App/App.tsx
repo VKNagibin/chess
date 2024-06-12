ac82c2ba3f88ea06adbada171f61bd9d12f7fd58
VKNagibin <itisnotstupid@gmail.com>
2024-06-12 15:50:07 +0300

    update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,13 @@
-import './App.less';
-
-import Cells from '@components/Cells';
-import Chars from '@components/Chars';
-import Numbers from '@components/Numbers';
+import { StyledApp } from '@components/App/styled';
+import Board from '@components/Board';
 import StepTeam from '@components/StepTeam';
 
 function App() {
   return (
-    <div className="app-container">
+    <StyledApp>
       <StepTeam />
-      <div className="board">
-        <Chars />
-        <Numbers />
-        <Cells />
-        <Numbers className="append" />
-        <Chars className="append" />
-      </div>
-    </div>
+      <Board />
+    </StyledApp>
   );
 }
 
