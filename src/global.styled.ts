aad1507e2b44996cfeed8e298c517209e771bd80
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:30:22 +0300

    update global.styled.ts

--- src/global.styled.ts
+++ src/global.styled.ts
@@ -8,17 +8,17 @@ export const GlobalStyles = createGlobalStyle`
   }
   
   #root {
-      height: 100vh;
+      min-height: 100vh;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       background-color: rgb(218, 216, 216);
-
   }
   
   body {
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    min-width: max-content;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
       'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
