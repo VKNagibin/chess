d6662785f5a151fc2f71f2b11586c7e1ebc650a9
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:33:41 +0300

    add Cells styles

---
+++ src/components/Cells/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledCellsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  background-color: white;
+  border: 10px solid black;
+  width: 640px;
+  height: 640px;
+  box-sizing: content-box;
+`;
