89dd5efd45b9d59c923469e92a6e977fbb1d6650
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:35:32 +0300

    add PawnMutateModal styles

---
+++ src/components/PawnMutateModal/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledButton = styled.button`
+  padding: 12px 24px;
+  display: flex;
+  font-size: 1.2rem;
+  color: darkslateblue;
+  border: none;
+  border-radius: 8px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: aquamarine;
+  justify-content: center;
+  align-items: center;
+`;
