e6ddf43839b100d28ce574ce87e536f95959cc6d
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:32:42 +0300

    update StepTeam component

--- src/components/StepTeam/index.tsx
+++ src/components/StepTeam/index.tsx
@@ -5,7 +5,7 @@ import { $currentStepTeam } from '@/stores/team';
 function StepTeam() {
   const currentStepTeam = useUnit($currentStepTeam);
 
-  return <h1 style={{ marginBottom: '80px' }}>{currentStepTeam}</h1>;
+  return <h1>{currentStepTeam}</h1>;
 }
 
 export default StepTeam;
