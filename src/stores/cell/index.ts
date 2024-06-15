47135acc88cfcb3549542fac75fcba91243b9dc6
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:31:06 +0300

    update cell store

--- src/stores/cell/index.ts
+++ src/stores/cell/index.ts
@@ -1,9 +1,10 @@
 import { createEvent, createStore, sample } from 'effector';
 
 import Cell from '@/entities/Cell/Cell';
-import { FigureTeam } from '@/entities/Cell/enums';
-import { CellIdType } from '@/entities/Cell/types';
-import { IStep } from '@/stores/cell/types';
+import { CELL_NUMBER } from '@/entities/Cell/constants';
+import { FigureTeam, FigureType } from '@/entities/Cell/enums';
+import Figure from '@/entities/Figure';
+import { ICellFocusHandler, ISelectedFigureForMutate, IStep } from '@/stores/cell/types';
 import arrangeCells from '@/stores/cell/utils/arrangeCells';
 import handleFigureSelect from '@/stores/cell/utils/handleFigureSelect';
 import { handleStep } from '@/stores/cell/utils/handleStep';
@@ -16,11 +17,10 @@ import {
   resetCellsHighlight,
 } from '@/stores/cell/utils/helpers';
 import { changeTeam, onGameOver } from '@/stores/events';
+import { onCloseMutateModal, onStartPawnMutate } from '@/stores/pawnMutateModal';
 
-export const onCellFocus = createEvent<{
-  cellId: CellIdType;
-  currentStepTeam: FigureTeam;
-}>();
+export const onCellFocus = createEvent<ICellFocusHandler>();
+export const onSelectFigureForMutate = createEvent<ISelectedFigureForMutate>();
 
 export const $cells = createStore<Cell[]>(arrangeCells());
 
@@ -32,7 +32,43 @@ sample({
   target: changeTeam,
 });
 
+sample({
+  clock: onCellFocus,
+  source: $cells,
+  filter: (cells) => !!cells.find((cell) => checkPawnReadyForMutate(cell)),
+  fn: (cells) => {
+    const cell = cells.find((cell) => checkPawnReadyForMutate(cell))!;
+    return {
+      cellId: cell.id,
+      team: cell.figure!.team,
+    };
+  },
+  target: onStartPawnMutate,
+});
+
+const teamConstraintRowMap = {
+  [FigureTeam.WHITE]: CELL_NUMBER.EIGHT,
+  [FigureTeam.BLACK]: CELL_NUMBER.ONE,
+};
+
+const checkPawnReadyForMutate = (cell: Cell) => {
+  const figure = cell.figure;
+  if (!figure) return false;
+  if (figure.type !== FigureType.PAWN) return false;
+  if (figure.isMutatedPawn) return false;
+  return teamConstraintRowMap[figure.team] === cell.id[1];
+};
+
 $cells.on(onGameOver, () => arrangeCells());
+
+$cells.on(onSelectFigureForMutate, (cells, { cellId, type }) => {
+  const currentCell = findById(cells, cellId) as Cell;
+  const team = currentCell.figure!.team;
+  return cells.map((cell) => {
+    if (cell.id === currentCell.id) return { ...cell, figure: new Figure(type, team) };
+    return cell;
+  });
+});
 
 $cells.on(onCellFocus, (cells, { cellId, currentStepTeam }): Cell[] => {
   const cellAlreadyFocused = findFocusedCell(cells)?.id === cellId;
