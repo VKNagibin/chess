582482adc4eaffb851cc192fbf3dc1e277e1e2df
v.nagibin <v.nagibin@worksolutions.ru>
2024-06-15 21:31:50 +0300

    update Board component

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,22 +1,20 @@
-import { StyledBoard, StyledBoardWrapper } from '@components/Board/styled';
+import { StyledBoardCenter, StyledBoardWrapper } from '@components/Board/styled';
 import Cells from '@components/Cells';
 import Chars from '@components/Chars';
 import Numbers from '@components/Numbers';
 
 function Board() {
-  return {};
-  //   <Numbers />
-  //   <StyledBoardWrapper>
-  //     <Chars />
-  //     <StyledBoard>
-  //       <Cells />
-  //     </StyledBoard>
-  //     <Numbers className="append" />
-  //     <Chars append />
-  //   </StyledBoardWrapper>
-  // <Numbers />
-
-  // );
+  return (
+    <StyledBoardWrapper>
+      <Numbers />
+      <StyledBoardCenter>
+        <Chars />
+        <Cells />
+        <Chars />
+      </StyledBoardCenter>
+      <Numbers />
+    </StyledBoardWrapper>
+  );
 }
 
 export default Board;
